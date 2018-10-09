f90bf0244c62896bcde731e9f9d356fb6a08aa40
Mathias Gottschlag <mgottschlag@gmail.com>
2018-10-09 07:34:11 +0200

    Added license/readme.

---
+++ README.md
@@ -0,0 +1,10 @@
+# `mkl25z4-hal`
+
+> HAL for NXP KL25Z microcontrollers
+
+# [Documentation](https://docs.rs/mkl25z4-hal)
+
+# License
+
+Licensed under the MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 // Copyright (c) 2012-2013 Andrew Payne
+//               2018 Mathias Gottschlag
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
